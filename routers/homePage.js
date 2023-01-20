67ea10975b10a27b94571ac9fb7de85d8a29fb43
Hlaing Htet <99160494+Hlaing-Htet@users.noreply.github.com>
2023-01-20 23:39:00 +0630

    home photo edit

--- routers/homePage.js
+++ routers/homePage.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const controller = require("../controllers/homePage");
 const router = express.Router();
-const { saveFile } = require("../utils/gallery");
+const { updateFile } = require("../utils/gallery");
 
 router.get("/", controller.getData);
 router.post("/", controller.addData);
 
-router.patch("/:id", saveFile, controller.updateData);
+router.patch("/:id", controller.updateData);
+router.patch("/:id/:fileName", updateFile, controller.updateData);
 
 module.exports = router;
--- utils/gallery.js
+++ utils/gallery.js
@@ -10,7 +10,16 @@ const saveFile = (req, res, next) => {
 const deleteFile = async (filename) => {
   await fs.unlinkSync(`./uploads/${filename}`);
 };
+const updateFile = (req, res, next) => {
+  deleteFile(req.params.fileName);
+  let file = req.files.file;
+  let filename = new Date().valueOf() + "_" + file.name;
+  file.mv(`./uploads/${filename}`);
+  req.body["image"] = filename;
+  next();
+};
 module.exports = {
   saveFile,
   deleteFile,
+  updateFile,
 };
